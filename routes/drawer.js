4b1cc2d9084da8d4f36329ecca055177d67ef116
Kipkorir74 <49982743+Kipkorir74@users.noreply.github.com>
2022-06-14 12:42:42 +0300

    More stying to the Home and Details Components

--- routes/drawer.js
+++ routes/drawer.js
@@ -16,28 +16,6 @@
 
 
 
-// const { Navigator, Screen } = createDrawerNavigator();
-
-// export const RootDrawerNavigator = () => (
-//   <Navigator initialRouteName='Home'>
-//     <Screen
-//       name='Home'
-//       component={HomeStack}
-//     />
-//     <Screen
-//       name='About'
-//       component={AboutStack}
-//     />
-//   </Navigator>
-// );
-
-// export const AppNavigator = () => (
-//   <NavigationContainer>
-//     <RootDrawerNavigator />
-//   </NavigationContainer>
-// );
-
-// export default AppNavigator;
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
